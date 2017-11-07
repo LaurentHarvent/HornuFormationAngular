46505db5915382cd04f79091a34ec805726cbf41
9072_angularjs (Student) <s_9072_angularjs@PEDAGOGIQUE.LAN>
2017-11-07 12:14:06 +0100

    add exercice01 folder

---
+++ exercice01/hello-world/src/app/user-item/user-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-user-item',
+  templateUrl: './user-item.component.html',
+  styleUrls: ['./user-item.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class UserItemComponent implements OnInit {
+  @Input() name: string;
+
+  constructor() {
+    this.name = 'Laurent';
+  }
+
+  ngOnInit() {
+  }
+
+}
